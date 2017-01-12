ca11e0f6a33d4698d99078c36c42477f9c0233f5
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2017-01-12 21:29:31 +0100

    configuring movieCell

--- EMDb/ListViewController.swift
+++ EMDb/ListViewController.swift
@@ -7,29 +7,55 @@
 //
 
 import UIKit
+import Kingfisher
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView : UICollectionView!
+    @IBOutlet weak var searchBar : UISearchBar!
+    
+    var movies : [Movie] = [Movie]()
+    var collectionViewPadding : CGFloat = 0
+    var tapGesture : UITapGestureRecognizer!
+    
+    let refresh = UIRefreshControl()
+    let dataProvider = LocalCoreDataService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupCollectionViewPadding() {
+        let screenWidth = self.view.frame.width
+        collectionViewPadding = (screenWidth - (3 * 113)) / 4
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(collectionViewPadding, collectionViewPadding, collectionViewPadding, collectionViewPadding)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return collectionViewPadding
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie  = movies[indexPath.row]
+        self.configureCell(cell, withMovie: movie)
+        
+        return cell
+    }
+    
+    func configureCell(_ cell: MovieCell, withMovie movie: Movie) {
+        if let imageData = movie.image {
+            cell.movieImage.kf.setImage(with: ImageResource(downloadURL: URL(string: imageData)!), placeholder: #imageLiteral(resourceName: "img-loading"), options: nil, progressBlock: nil, completionHandler: nil)
+        }
+    }
 }
---
+++ EMDb/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  EMDb
+//
+//  Created by Benjamín Garrido Barreiro on 12/1/17.
+//  Copyright © 2017 Benjamín Garrido Barreiro. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    @IBOutlet weak var movieImage : UIImageView!
+}
