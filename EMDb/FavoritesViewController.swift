70ae574782803afaebd3f6aad11cf4ebaf556794
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2017-01-14 13:29:20 +0100

    programming favoritesViewController methods

--- EMDb/FavoritesViewController.swift
+++ EMDb/FavoritesViewController.swift
@@ -7,29 +7,78 @@
 //
 
 import UIKit
+import Kingfisher
 
-class FavoritesViewController: UIViewController {
+class FavoritesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView : UICollectionView!
+    
+    var collectionViewPadding : CGFloat = 0
+    var movies: [Movie] = [Movie]()
+    let dataProvider = LocalCoreDataService()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        setupCollectionViewPadding()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupCollectionViewPadding() {
+        let screenWidth = self.view.frame.width
+        collectionViewPadding = (screenWidth - (3 * 113)) / 4
     }
-    */
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(collectionViewPadding, collectionViewPadding, collectionViewPadding, collectionViewPadding)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return collectionViewPadding
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 113, height: 170)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if movies.count > 0 {
+            self.collectionView.backgroundView = nil
+        } else {
+            let imageView = UIImageView(image: UIImage(named: "sin-favoritas"))
+            imageView.contentMode = .center
+            self.collectionView.backgroundView = imageView
+        }
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie  = movies[indexPath.row]
+        self.configureCell(cell, withMovie: movie)
+        
+        return cell
+    }
+    
+    func configureCell(_ cell: MovieCell, withMovie movie: Movie) {
+        if let imageData = movie.image {
+            cell.movieImage.kf.setImage(with: ImageResource(downloadURL: URL(string: imageData)!), placeholder: #imageLiteral(resourceName: "img-loading"), options: nil, progressBlock: nil, completionHandler: nil)
+        }
+    }
+    
+    func loadData() {
+        if let movies = dataProvider.getFavoriteMovies() {
+            self.movies = movies
+            self.collectionView.reloadData()
+        }
+    }
 }
